7d85fb86b6be161a17c20d87bb24ab60db61ef8b
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2021-11-22 00:31:28 -0500

    weather displayin by city name and also displaying the city list underneath search menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,76 @@
 //API-KEY  -------->>> 19881ccddec89e1f1d5f5979ebc7fa0a
 //this will make api call and get the result
 var apiId = "19881ccddec89e1f1d5f5979ebc7fa0a"
-
+var city;
+var listCity = []
 //make api call from lat,lon to get the result
 function getWeather(lat,lon){
-   fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" +lat + "&lon=" + lon + "&APPID=" + apiId)
+   fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" +lat + "&lon=" + lon + "&units=imperial&APPID=" + apiId)
    .then(function(response){
        return response.json()
    }).then(function(res){
-       console.log(res)
+        updateCurrentDayEl(res)
    })
 }
+//this will create dynamic current day specs
+function updateCurrentDayEl(res){
+
+    $(".currentDayHeading h2").html(`${city.val()} (${moment().format("DD/MM/YYYY")})`)
+    console.log(`Temp: ${res.current.temp}` )
+    console.log(`Wind: ${res.current.wind_speed}` )
+    console.log(`Humidity: ${res.current.humidity}` )
+    console.log(`UV index: ${res.current.uvi}` )
+    var specs = {
+        "Temp" : res.current.temp, 
+        "Wind" : res.current.wind_speed, 
+        "Humidity" : res.current.humidity,
+        "UV index" : res.current.uvi
+    }
+    
+    var specsContainer = $("<div>")
+                        .addClass("currentDaySpecs")
+    
+    var liELCoantainer = $("<ul>")
+                        .addClass("specsUl p-0")
+
+    for(let v=0;v<4;v++){
+        var li = $("<li>")
+                 .addClass("liEl liItems h3")
+                 .html( `${Object.keys(specs)[v]} : ${Object.values(specs)[v]}`)
+        liELCoantainer.append(li)
+    }
+
+    //specsContainer.append(liELCoantainer)
+    $(".currentDay ul").replaceWith(liELCoantainer)
+    city.val("")
+    addCityNames();
+}
+//adding city names to the list beneath the search Menu
+function addCityNames(){
+
+
+    var div = $("<div>")
+              .addClass("cityList mt-4 border-top")
+
+    listCity.forEach((el) => {
+        var cityNames = $("<a>")
+                        .addClass("h3 mx-auto d-block")
+                        .attr("href","https://google.com")
+                        .html(el)
+    
+        div.append(cityNames)
+        $(".cityList").replaceWith(div)
+    })
+    
+
+}
+
 //make an api call to get lat, lon from the city name
 function getLatLon(city){
 
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiId)
     .then(function(response){
         if(response.ok){
-            console.log("another")
             response.json().then(function(res){
                 getWeather(res.coord.lat,res.coord.lon)
             })
@@ -29,10 +82,20 @@ function getLatLon(city){
 }
 
 $("#submitCity").on("click", function(e){
-    var city = $("#city")
     e.preventDefault()
+    city = $("#city")
+    
+    if(!city.val()){
+        console.log("empty city name")
+        return;
+    }
     getLatLon(city.val())
-    city.val("")
+    listCity.push(city.val())
 })
 
-console.log("refreshed")
+
+
+
+
+
+$("#submitCity").val("Search")
